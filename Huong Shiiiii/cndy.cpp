98d7935d7be72b3ad541df2900b1231fa5dd338a
faxilhs <99594438+faxilhs@users.noreply.github.com>
2022-12-02 13:52:14 +0800

    Add files via upload

---
+++ Huong Shiiiii/cndy.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+	bool result = true;
+	int N=0;
+	cin >> N;
+	
+	int arr[100] = {};
+	int A = 2 * N;
+	for (size_t i = 0; i < A; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (size_t i = 0; i < A; i++)
+	{
+		int c = 1;
+		for (size_t j = i+1; j < A; j++)
+		{
+			if (arr[i] == arr[j]) {
+				c++;
+			}
+			if (c == 3) {
+				result = false;
+				break;
+			}
+		}
+		if (result == false) {
+			break;
+		}
+	}
+	cout << (result ? "Yes" : "No") << endl;
+}
+
+int main() {
+	int T = 1;
+	cin >> T;
+	while (T--) {
+		solve();
+	}
+}
