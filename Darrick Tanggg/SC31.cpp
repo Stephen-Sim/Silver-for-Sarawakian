190cff793277be6398dd8febc6d802c552b8c157
DARRICK TANG <105955133+DARRICK1103@users.noreply.github.com>
2022-12-04 22:59:57 +0800

    Update SC31.cpp

--- Darrick Tanggg/SC31.cpp
+++ Darrick Tanggg/SC31.cpp
@@ -10,25 +10,26 @@ void solve()
 	for (auto i = 0; i < n; i++)
 	{
 		cin >> s;
-		for (int j = 0; j <10; j++)
+		for (int j = 0; j < 10; j++)
 		{
-			
-			if (s[j] == '1')
+
+			if (s[j] == '1' && weapon[j] == 1 || s[j] == '0' && weapon[j] == 0)
 			{
-				if (weapon[j] == 0)
-					weapon[j] = 1;
-				else
-					weapon[j] = 0;
+				weapon[j] = 0;
+			}
+			else
+			{
+				weapon[j] = 1;
 			}
 		}
-		
+
 	}
 
 	for (auto i = 1; i < 10; i++)
 	{
-		weapon[i] += weapon[i-1];
+		weapon[i] += weapon[i - 1];
 	}
-	
+
 	cout << weapon[9] << endl;
 }
 
@@ -36,10 +37,10 @@ int main()
 {
 	int x = 1;
 	cin >> x;
-	while (x --)
+	while (x--)
 	{
 		solve();
-		
+
 	}
 	return 0;
 }
