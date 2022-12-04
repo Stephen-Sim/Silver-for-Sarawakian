db3b21ad927b883ec8dea12e2dd1e09f0aa84b0a
DARRICK TANG <105955133+DARRICK1103@users.noreply.github.com>
2022-12-04 22:54:00 +0800

    Update PROGLANG.cpp

--- Darrick Tanggg/PROGLANG.cpp
+++ Darrick Tanggg/PROGLANG.cpp
@@ -10,38 +10,19 @@ void solve()
 		
 	}
 
-	if (arr[3] == arr[0])
+	if ((arr[3] == arr[0] && arr[2] == arr[1] )|| (arr[3] == arr[1] && arr[2] == arr[0]))
 	{
 		
-		if (arr[2] == arr[1])
-		{
-			cout << "1" << endl;
-			
-		}
+		cout << "1" << endl;
 
 	}
-	else if (arr[3] == arr[1])
+	else if ((arr[5] == arr[0] && arr[4] == arr[1]) || (arr[5] == arr[1] && arr[4] == arr[0]))
 	{
 	
-		if (arr[2] == arr[0])
-		{
-			cout << "1" << endl;
-			
-		}
+		cout << "1" << endl;
 
 	}
-	else if (arr[5] == arr[0])
-	{
-		
-		if (arr[4] == arr[1])
-			cout << "2" << endl;
-	}
-	else if (arr[5] == arr[1])
-	{
-		
-		if (arr[4] == arr[0])
-			cout << "2" << endl;
-	}
+	
 	else
 		cout << "0" << endl;
 
